06740998892952043334308d70939c205209428c
johnservo <johnservo.se@gmail.com>
2024-05-24 20:59:53 +0800

    Added comments

--- CowSayCaller.cs
+++ CowSayCaller.cs
@@ -10,12 +10,15 @@ class CowSay
             {
                 FileName = "/bin/bash",
                 ArgumentList = { "-c", $"cowsay {content}" },
+                // Stdout gets redirected to our program instead of being displayed in the terminal
                 RedirectStandardOutput = true,
             }
         };
 
         process.Start();
         string output = process.StandardOutput.ReadToEnd();
+
+        // Wait for exit because we need to get all output first before proceeding w/ the rest of the program.
         process.WaitForExit();
 
         Console.WriteLine(output);
